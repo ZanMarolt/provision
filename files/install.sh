dd092dbf8a709f036451a56acc10f57ab195b6c1
Zan Marolt <marolt@zan.sh>
2022-09-15 15:32:01 +0200

    remove sudo mode for initial install

--- files/install.sh
+++ files/install.sh
@@ -1,9 +1,3 @@
-# Require eleated permisions
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root, use sudo "$0" instead" 1>&2
-   exit 1
-fi
-
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
